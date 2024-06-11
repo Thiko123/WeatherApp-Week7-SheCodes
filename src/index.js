a978212af61ff6c4870442ae19926994f2aec1e4
Thiko123 <thikonetshiongolwe@gmail.com>
2024-06-11 02:16:45 +0200

    semi-draft for API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function refreshWeather(response){
+    let temperatureElement= document.querySelector("#temperature")
+    let temperature = response.data.temperature.current;
+    let cityElement =document.querySelector("#city");
+
+    cityElement.innerHTML= response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+
+function searchCity(city){
+    let apiKey = `b430f3eb1cfa45o92ta170b00b8665fa`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiKey).then(refreshWeather);
+}
+
 function searchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement =document.querySelector("#city");
-    cityElement.innerHTML= searchInput.value;
+    searchCity(searchInput.value);
 }
 let searchforForm = document.querySelector("#search-form");
-searchforForm.addEventListener("submit", searchSubmit)
+searchforForm.addEventListener("submit", searchSubmit);
