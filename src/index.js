7574b86b47abe1e312cac26fc47a5934a8795efe
Thiko123 <thikonetshiongolwe@gmail.com>
2024-06-12 16:55:19 +0200

    completed with the forecast

--- src/index.js
+++ src/index.js
@@ -60,6 +60,13 @@ function getForecast(city){
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
     axios(apiUrl).then(displayForecast);
 }
+
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+    return days[date.getDay()];
+}
 function displayForecast(response){
 let days = [
     "Sun",
@@ -72,19 +79,21 @@ let days = [
   ];
   let forecastHtml="";
 
-  days.forEach(function(day){
+  response.data.daily.forEach(function(day, index){
+    if (index<5){
 forecastHtml= forecastHtml+
 `<div class="weather-forecast-day">
-<div class="weather-forecast-date">${day}</div>
-<div class="weather-forecast-icon">🌤️</div>
+<div class="weather-forecast-date">${formatDay(day.time)}</div>
+<img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
 <div class="weather-forecast-temperatures">
   <span class="weather-forecast-temperature">
-    <strong> 15º </strong>
+    <strong> ${Math.round(day.temperature.maximum)}º </strong>
   </span>
-  <span class="weather-forecast-temperature"> 9º </span>
+  <span class="weather-forecast-temperature"> ${Math.round(day.temperature.minimum)}º </span>
 
 </div>
 </div>`;
+}
 });
 let forecastElement = document.querySelector("#forecast");
 forecastElement.innerHTML= forecastHtml;
