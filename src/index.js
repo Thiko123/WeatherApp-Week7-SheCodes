ce3b66c353aa1559208c1742f2af7ca3fb94606e
Thiko123 <thikonetshiongolwe@gmail.com>
2024-06-12 04:12:42 +0200

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,12 +2,41 @@ function refreshWeather(response){
     let temperatureElement= document.querySelector("#temperature")
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
-
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#Humidity");
+    let windElement = document.querySelector("#Wind");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+ 
     cityElement.innerHTML= response.data.city;
+    timeElement.innerHTML = formatDate(date);
     temperatureElement.innerHTML = Math.round(temperature);
+    descriptionElement.innerHTML= response.data.condition.description;
+    humidityElement.innerHTML= `${response.data.temperature.humidity}%`;
+    windElement.innerHTML= `${response.data.wind.speed}km/h`;
+
 
 }
-
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+  
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    return `${day} ${hours}:${minutes}`;
+  }
 
 function searchCity(city){
     let apiKey = `b430f3eb1cfa45o92ta170b00b8665fa`;
